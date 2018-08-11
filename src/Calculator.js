036d870e4bad43f8283f10fbebbb9cf77f20348e
Kevin Win <mail@kevinwin.com>
2018-08-10 22:13:56 -0700

    Add handlers for numbers, reset, sign toggle, and percent

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import './Calculator.css';
 
-const Calculator = (props) => (
-  <div className="Calculator">
-    <div className="display-wrapper">
-      <div className="display">{props.total}</div>
+const Calculator = (props) => {
+  const { total, memory, handleNumClick, handleResetClick, handlePercentClick, handleToggleSignClick } = props;
+  return (
+    <div className="Calculator">
+      <div className="display-wrapper">
+        <div className="display">{total}</div>
+      </div>
+      <div className="reset" onClick={ () => handleResetClick() }>{memory ? 'C' : 'AC'}</div>
+      <div className="sign-toggler" onClick={ () => handleToggleSignClick() }>&plusmn;</div>
+      <div className="percent" onClick={() => handlePercentClick() }>%</div>
+      <div className="num-zero-wrapper" onClick={ () => handleNumClick('0') }>
+        <div className="num-zero">0</div>
+      </div>
+      <div className="num-one" onClick={ () => handleNumClick('1') } >1</div>
+      <div className="num-two" onClick={ () => handleNumClick('2') }>2</div>
+      <div className="num-three" onClick={ () => handleNumClick('3') }>3</div>
+      <div className="num-four" onClick={ () => handleNumClick('4') }>4</div>
+      <div className="num-five" onClick={ () => handleNumClick('5') }>5</div>
+      <div className="num-six" onClick={ () => handleNumClick('6') }>6</div>
+      <div className="num-seven" onClick={ () => handleNumClick('7') }>7</div>
+      <div className="num-eight" onClick={ () => handleNumClick('8') }>8</div>
+      <div className="num-nine" onClick={ () => handleNumClick('9') }>9</div>
+      <div className="sign-divide">&divide;</div>
+      <div className="sign-multiply">&times;</div>
+      <div className="sign-subtract">-</div>
+      <div className="sign-add">+</div>
+      <div className="sign-equal">=</div>
+      <div className="sign-decimal" onClick={ () => handleNumClick('.') }>.</div>
     </div>
-    <div className="reset">AC</div>
-    <div className="sign-toggler">&plusmn;</div>
-    <div className="percent">%</div>
-    <div className="num-zero-wrapper">
-      <div className="num-zero">0</div>
-    </div>
-    <div className="num-one">1</div>
-    <div className="num-two">2</div>
-    <div className="num-three">3</div>
-    <div className="num-four">4</div>
-    <div className="num-five">5</div>
-    <div className="num-six">6</div>
-    <div className="num-seven">7</div>
-    <div className="num-eight">8</div>
-    <div className="num-nine">9</div>
-    <div className="sign-divide">&divide;</div>
-    <div className="sign-multiply">&times;</div>
-    <div className="sign-subtract">-</div>
-    <div className="sign-add">+</div>
-    <div className="sign-equal">=</div>
-    <div className="sign-decimal">.</div>
-  </div>
-);
+  );
+
+}
 
 export default Calculator;
-  
+
 
